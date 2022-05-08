f82450586c3eca017fad6a35c064e50dc6efd576
snozaki <snozaki@netprotections.co.jp>
2022-05-08 22:07:30 +0900

    MOD: コントローラのレスポンスを返す

--- src/promissory_notes_platform/handler/settlement_controller.clj
+++ src/promissory_notes_platform/handler/settlement_controller.clj
@@ -1,17 +1,23 @@
 (ns promissory-notes-platform.handler.settlement-controller
+  (:require [promissory-notes-platform.application.consts.error-const :refer [error-messages]])
   (:use [integrant.core :as ig]
         [ataraxy.response :as response]
         [promissory-notes-platform.usecase.usecase :as usecase]
-        [taoensso.timbre :as timbre]))
+        [taoensso.timbre :as timbre])
+  (:import (java.time LocalDateTime)))
 
 (defmethod ig/init-key ::execute [_ {:keys [settlement-usecase]}]
   "代金決済コントローラ"
   (fn [{[_ params] :ataraxy/result}]
-    (timbre/info str "リクエスト =>" params)
+    (timbre/info "リクエスト =>" params)
     (try
-      (let [result (usecase/execute settlement-usecase params)]
+      (let [result (usecase/execute settlement-usecase params)
+            response (if (nil? result)
+                       {:message (:e.domain.value-object.spec-error error-messages)}
+                       {})]
         (timbre/info "決済完了 =>" result)
-        (::response/ok {}))
+        (timbre/info "レスポンス => " response)
+        [::response/ok (assoc response :settled-at (LocalDateTime/now))])
       (catch Exception e
         (timbre/error "予期しないエラー" (.printStackTrace e))
-        (::response/internal-server-error {:message "unexpected error"})))))
+        [::response/internal-server-error {:message (:e.system.unexpected-error error-messages)}]))))
