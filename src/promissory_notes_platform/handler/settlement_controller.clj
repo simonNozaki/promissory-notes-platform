dd3f6b6b95ca230145c8f1b3350ac23219679da6
snozaki <snozaki@netprotections.co.jp>
2022-05-08 15:46:19 +0900

    MOD: 例外を処理する

--- src/promissory_notes_platform/handler/settlement_controller.clj
+++ src/promissory_notes_platform/handler/settlement_controller.clj
@@ -1,10 +1,17 @@
 (ns promissory-notes-platform.handler.settlement-controller
   (:use [integrant.core :as ig]
         [ataraxy.response :as response]
-        [promissory-notes-platform.usecase.usecase :as usecase]))
+        [promissory-notes-platform.usecase.usecase :as usecase]
+        [taoensso.timbre :as timbre]))
 
 (defmethod ig/init-key ::execute [_ {:keys [settlement-usecase]}]
+  "代金決済コントローラ"
   (fn [{[_ params] :ataraxy/result}]
-    (println params)
-    (usecase/execute settlement-usecase params)
-    (::response/ok #{})))
+    (timbre/info str "リクエスト =>" params)
+    (try
+      (let [result (usecase/execute settlement-usecase params)]
+        (timbre/info "決済完了 =>" result)
+        (::response/ok {}))
+      (catch Exception e
+        (timbre/error "予期しないエラー" (.printStackTrace e))
+        (::response/internal-server-error {:message "unexpected error"})))))
