f62178255ad59cfc6b6edd4946dadfe9366e90d4
snozaki <snozaki@netprotections.co.jp>
2022-05-28 16:01:51 +0900

    ADD: DAOレイヤの追加

--- src/promissory_notes_platform/infrastructure/repository/bank_repository.clj
+++ src/promissory_notes_platform/infrastructure/repository/bank_repository.clj
@@ -4,16 +4,17 @@
         [promissory-notes-platform.domain.account.model]
         [promissory-notes-platform.domain.account.value-object]
         [integrant.core :as ig]
-        [clojure.spec.alpha :as s]))
+        [clojure.spec.alpha :as s]
+        [promissory-notes-platform.infrastructure.dao.bank-dao :as dao]))
 
 ; データベースからデータをとってくるとして、DAOレイヤを用意する
-(defrecord InMemoryBankRepository []
+(defrecord InMemoryBankRepository [bank-dao]
   BankRepository
   (find [this bank branch type number holder]
     (let [account-number (if (s/valid? :promissory-notes-platform.domain.account.value-object/number number)
                            number
                            "000000")]
-      (->Bank bank branch (->Account holder :normal account-number 1000000)))))
+      (dao/findByBank bank-dao bank branch type account-number holder))))
 
-(defmethod ig/init-key ::inmemory-bank-repository [_ {}]
-  (->InMemoryBankRepository))
+(defmethod ig/init-key ::inmemory-bank-repository [_ {:keys [bank-dao]}]
+  (->InMemoryBankRepository bank-dao))
